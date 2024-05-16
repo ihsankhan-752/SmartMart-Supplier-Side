0343d64df967e9c5b120912a3c71d9fc0b277843
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-05-17 01:09:43 +0500

    Seller Profile Update image portion done

--- lib/screens/custom_navbar/settings/my_profile/my_profile.dart
+++ lib/screens/custom_navbar/settings/my_profile/my_profile.dart
@@ -1,18 +1,30 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_mart_supplier_side/constants/colors.dart';
-import 'package:smart_mart_supplier_side/screens/custom_navbar/settings/my_profile/widgets/show_edit_profile_widget.dart';
-import 'package:smart_mart_supplier_side/screens/custom_navbar/settings/my_profile/widgets/title_value_widget.dart';
+import 'package:smart_mart_supplier_side/controllers/loading_controller.dart';
 import 'package:smart_mart_supplier_side/widgets/buttons.dart';
+import 'package:smart_mart_supplier_side/widgets/text_input.dart';
 
-import '../../../../constants/app_assets.dart';
+import '../../../../constants/text_styles.dart';
 import '../../../../controllers/image_controller.dart';
 import '../../../../controllers/seller_controller.dart';
+import '../../../../services/seller_profile_services.dart';
 import '../../widgets/custom_appbar_header.dart';
 
-class MyProfileScreen extends StatelessWidget {
+class MyProfileScreen extends StatefulWidget {
   const MyProfileScreen({super.key});
 
+  @override
+  State<MyProfileScreen> createState() => _MyProfileScreenState();
+}
+
+class _MyProfileScreenState extends State<MyProfileScreen> {
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final sellerController = Provider.of<SellerController>(context);
@@ -22,7 +34,7 @@ class MyProfileScreen extends StatelessWidget {
         child: Column(
           children: [
             CustomAppBarHeader(
-              title: "My Profile",
+              title: "Edit Profile",
               widget: GestureDetector(
                 onTap: () {
                   Navigator.pop(context);
@@ -32,57 +44,46 @@ class MyProfileScreen extends StatelessWidget {
             ),
             SizedBox(height: 40),
             Center(
-              child: sellerController.sellerModel.image == ""
-                  ? Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundColor: AppColors.primaryColor,
-                          child: Center(
-                            child: Padding(
-                              padding: const EdgeInsets.all(25.0),
-                              child: Image.asset(AppAssets.profileIcon, color: AppColors.primaryWhite),
-                            ),
-                          ),
-                        ),
-                        // Positioned(
-                        //   bottom: 0,
-                        //   right: -10,
-                        //   child: GestureDetector(
-                        //     onTap: () {
-                        //       imageController.uploadImage(ImageSource.gallery);
-                        //     },
-                        //     child: CircleAvatar(
-                        //       radius: 18,
-                        //       backgroundColor: AppColors.primaryWhite,
-                        //       child: Center(
-                        //         child: Icon(Icons.image, size: 20),
-                        //       ),
-                        //     ),
-                        //   ),
-                        // )
-                      ],
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  if (imageController.selectedImage == null && sellerController.sellerModel.image == "")
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: AppColors.primaryColor,
+                      child: Icon(Icons.person, size: 45, color: AppColors.primaryWhite),
                     )
-                  : Stack(
-                      children: [
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundImage: NetworkImage(sellerController.sellerModel.image!),
-                        ),
-                        Positioned(
-                          bottom: 0,
-                          right: -10,
-                          child: CircleAvatar(
-                            radius: 18,
-                            backgroundColor: AppColors.primaryWhite,
-                            child: Center(
-                              child: Icon(Icons.image, size: 20),
-                            ),
-                          ),
-                        )
-                      ],
+                  else if (imageController.selectedImage == null && sellerController.sellerModel.image != "")
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: AppColors.primaryColor,
+                      backgroundImage: NetworkImage(sellerController.sellerModel.image!),
+                    )
+                  else
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: AppColors.primaryColor,
+                      backgroundImage: FileImage(File(imageController.selectedImage!.path)),
                     ),
+                  Positioned(
+                    bottom: 0,
+                    right: -10,
+                    child: GestureDetector(
+                      onTap: () {
+                        imageController.uploadImage(ImageSource.gallery);
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(shape: BoxShape.circle, color: AppColors.grey),
+                        child: Center(
+                          child: Icon(Icons.camera_alt, size: 20, color: AppColors.primaryWhite),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
             SizedBox(height: 40),
             Container(
@@ -97,10 +98,29 @@ class MyProfileScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    TitleValueWidget(title: "Name", value: sellerController.sellerModel.sellerName!),
-                    TitleValueWidget(title: "E-mail", value: sellerController.sellerModel.email!),
-                    TitleValueWidget(title: "Contact", value: sellerController.sellerModel.phone!.toString()),
-                    TitleValueWidget(title: "Address", value: sellerController.sellerModel.address!),
+                    Text("Profile Information", style: AppTextStyles().H2.copyWith(fontSize: 16)),
+                    SizedBox(height: 20),
+                    Text("Name", style: AppTextStyles().H2.copyWith(fontSize: 14)),
+                    SizedBox(height: 10),
+                    CustomTextInput(
+                      controller: _nameController,
+                      hintText: sellerController.sellerModel.sellerName!,
+                    ),
+                    SizedBox(height: 20),
+                    Text("Contact", style: AppTextStyles().H2.copyWith(fontSize: 14)),
+                    SizedBox(height: 10),
+                    CustomTextInput(
+                      inputType: TextInputType.number,
+                      controller: _phoneController,
+                      hintText: sellerController.sellerModel.phone.toString(),
+                    ),
+                    SizedBox(height: 20),
+                    Text("Address", style: AppTextStyles().H2.copyWith(fontSize: 14)),
+                    SizedBox(height: 10),
+                    CustomTextInput(
+                      controller: _addressController,
+                      hintText: sellerController.sellerModel.address,
+                    ),
                   ],
                 ),
               ),
@@ -108,18 +128,31 @@ class MyProfileScreen extends StatelessWidget {
             SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-              child: PrimaryButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return ShowEditProfileWidget(sellerModel: sellerController.sellerModel);
-                      });
-                },
-                title: "Edit Profile",
-                btnColor: AppColors.primaryColor,
-                width: double.infinity,
-              ),
+              child: Consumer<LoadingController>(builder: (context, loadingController, child) {
+                return loadingController.isLoading
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : PrimaryButton(
+                        onPressed: () {
+                          SellerProfileServices().updateSellerInfo(
+                            context: context,
+                            sellerName:
+                                _nameController.text.isEmpty ? sellerController.sellerModel.sellerName! : _nameController.text,
+                            phone: _phoneController.text.isEmpty
+                                ? sellerController.sellerModel.phone!
+                                : int.tryParse(_phoneController.text)!,
+                            address:
+                                _addressController.text.isEmpty ? sellerController.sellerModel.address! : _addressController.text,
+                            image: imageController.selectedImage,
+                          );
+                          imageController.removeUploadPhoto();
+                        },
+                        title: "Save Changes",
+                        btnColor: AppColors.primaryColor,
+                        width: double.infinity,
+                      );
+              }),
             )
           ],
         ),
--- lib/services/seller_profile_services.dart
+++ lib/services/seller_profile_services.dart
@@ -1,9 +1,14 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:smart_mart_supplier_side/controllers/loading_controller.dart';
+import 'package:smart_mart_supplier_side/services/storage_services.dart';
 import 'package:smart_mart_supplier_side/widgets/custom_msg.dart';
+
+import '../constants/image_compressor.dart';
 
 class SellerProfileServices {
   Future<void> updateSellerInfo({
@@ -11,13 +16,22 @@ class SellerProfileServices {
     required String sellerName,
     required int phone,
     required String address,
+    File? image,
   }) async {
     try {
       Provider.of<LoadingController>(context, listen: false).setLoading(true);
+      DocumentSnapshot snap =
+          await FirebaseFirestore.instance.collection('sellers').doc(FirebaseAuth.instance.currentUser!.uid).get();
+      String? imageUrl;
+      if (image != null) {
+        File _compressImage = await compressImage(image);
+        imageUrl = await StorageServices().uploadImageToStorage(context, _compressImage);
+      }
       await FirebaseFirestore.instance.collection('sellers').doc(FirebaseAuth.instance.currentUser!.uid).update({
         'sellerName': sellerName,
         'address': address,
         'phone': phone,
+        'image': imageUrl ?? snap['image'],
       });
 
       Provider.of<LoadingController>(context, listen: false).setLoading(false);
