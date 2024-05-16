d72f67d9258d448d4077227a83f6ae3d2657e8cc
ihsankhan-752 <ihsankhan.se752@gmail.com>
2024-05-17 00:50:20 +0500

    Buttons Added to Order Detail Page for updating order status

--- lib/screens/custom_navbar/orders/order_detail_page.dart
+++ lib/screens/custom_navbar/orders/order_detail_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
+import 'package:smart_mart_supplier_side/controllers/loading_controller.dart';
 import 'package:smart_mart_supplier_side/controllers/seller_controller.dart';
 import 'package:smart_mart_supplier_side/model/order_model.dart';
 import 'package:smart_mart_supplier_side/screens/custom_navbar/orders/widgets/customer_information_card.dart';
 import 'package:smart_mart_supplier_side/screens/custom_navbar/orders/widgets/product_information_card.dart';
 import 'package:smart_mart_supplier_side/screens/custom_navbar/orders/widgets/production_images_portion.dart';
 import 'package:smart_mart_supplier_side/screens/custom_navbar/widgets/custom_appbar_header.dart';
+import 'package:smart_mart_supplier_side/services/order_services.dart';
+import 'package:smart_mart_supplier_side/widgets/buttons.dart';
+import 'package:smart_mart_supplier_side/widgets/custom_msg.dart';
+
+import '../../../constants/colors.dart';
 
 class OrderDetailScreen extends StatefulWidget {
   final OrderModel orderModel;
@@ -31,21 +37,94 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CustomAppBarHeader(
-            widget: GestureDetector(
-              onTap: () {
-                Get.back();
-              },
-              child: Icon(Icons.arrow_back),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomAppBarHeader(
+              widget: GestureDetector(
+                onTap: () {
+                  Get.back();
+                },
+                child: Icon(Icons.arrow_back),
+              ),
+              title: "Order Detail",
             ),
-            title: "Order Detail",
-          ),
-          ProductImagesPortion(orderModel: widget.orderModel),
-          ProductInformationCard(orderModel: widget.orderModel),
-          CustomerInformationCard(),
-        ],
+            ProductImagesPortion(orderModel: widget.orderModel),
+            ProductInformationCard(orderModel: widget.orderModel),
+            CustomerInformationCard(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+              child: Consumer<LoadingController>(builder: (context, loadingController, child) {
+                return loadingController.isLoading
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          if (widget.orderModel.orderStatus != 'cancel')
+                            SmallButton(
+                              onPressed: () {
+                                if (widget.orderModel.orderStatus == 'preparing') {
+                                  OrderServices.updateOrderStatus(
+                                    context: context,
+                                    orderId: widget.orderModel.orderId!,
+                                    orderStatus: 'shipment',
+                                  );
+                                } else if (widget.orderModel.orderStatus == 'shipment') {
+                                  OrderServices.updateOrderStatus(
+                                    context: context,
+                                    orderId: widget.orderModel.orderId!,
+                                    orderStatus: 'deliver',
+                                  );
+                                } else {
+                                  showCustomMessage(context, "Order Delivered");
+                                }
+                              },
+                              width: widget.orderModel.orderStatus == 'deliver' ? Get.width - 30 : Get.width * 0.43,
+                              height: 50,
+                              btnColor: AppColors.primaryColor,
+                              textColor: AppColors.primaryWhite,
+                              title: widget.orderModel.orderStatus == 'preparing'
+                                  ? "Proceed for Shipment"
+                                  : widget.orderModel.orderStatus == 'shipment'
+                                      ? 'Proceed To Deliver'
+                                      : "Delivered",
+                            )
+                          else
+                            SmallButton(
+                              onPressed: () {
+                                showCustomMessage(context, "Order Canceled");
+                              },
+                              height: 50,
+                              width: Get.width - 30,
+                              btnColor: Colors.red[700],
+                              textColor: AppColors.primaryWhite,
+                              title: "Order Canceled",
+                            ),
+                          if (widget.orderModel.orderStatus == 'preparing' || widget.orderModel.orderStatus == 'shipment')
+                            SmallButton(
+                              onPressed: () {
+                                OrderServices.updateOrderStatus(
+                                  context: context,
+                                  orderId: widget.orderModel.orderId!,
+                                  orderStatus: 'cancel',
+                                );
+                              },
+                              height: 50,
+                              width: Get.width * 0.43,
+                              btnColor: Colors.red[700],
+                              textColor: AppColors.primaryWhite,
+                              title: "Cancel Order",
+                            )
+                          else
+                            SizedBox(),
+                        ],
+                      );
+              }),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/services/order_services.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:provider/provider.dart';
+import 'package:smart_mart_supplier_side/controllers/loading_controller.dart';
+import 'package:smart_mart_supplier_side/widgets/custom_msg.dart';
+
+class OrderServices {
+  static updateOrderStatus({required BuildContext context, required String orderId, required String orderStatus}) async {
+    try {
+      Provider.of<LoadingController>(context, listen: false).setLoading(true);
+
+      await FirebaseFirestore.instance.collection('orders').doc(orderId).update({
+        'orderStatus': orderStatus,
+      });
+      Provider.of<LoadingController>(context, listen: false).setLoading(false);
+      Get.back();
+    } on FirebaseException catch (e) {
+      Provider.of<LoadingController>(context, listen: false).setLoading(false);
+
+      showCustomMessage(context, e.message!);
+    }
+  }
+}
